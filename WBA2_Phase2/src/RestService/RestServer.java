a493d20c8336485935b0e6332a0c2b66ff763eec
dennis.jga@gmail.com <juPtdvnIAS7io3xsJApGSGPZI>
2013-05-16 16:45:10 +0200

    REST_Server optimiert

--- WBA2_Phase2/src/RestService/RestServer.java
+++ WBA2_Phase2/src/RestService/RestServer.java
@@ -9,9 +9,8 @@ import java.util.Scanner;
 public class RestServer {
 
 
-    public static void main(String[] args) throws IllegalArgumentException,IOException {
+    public static void main(String[] args) throws IllegalArgumentException, IOException {
         String url = "http://localhost:4434";
-
         SelectorThread srv = GrizzlyServerFactory.create(url);
         System.out.println("Server gestartet ... [ENTER]");
         (new Scanner(System.in)).nextLine();
