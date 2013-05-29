ddda27c59caeb9223685ba59c4e3a944fce6294e
dennis.jga@gmail.com <juPtdvnIAS7io3xsJApGSGPZI>
2013-05-29 13:40:04 +0200

    XMPP_Server Entwicklung

---
+++ WBA2_Phase2/src/xmpp/XmppService.java
@@ -0,0 +1,41 @@
+package xmpp;
+
+
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smackx.pubsub.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: djga
+ * Date: 28.05.13
+ * Time: 17:09
+ * To change this template use File | Settings | File Templates.
+ */
+public class XmppService {
+    public static void main(String[] args) throws XMPPException {
+        XMPPConnection connection = new XMPPConnection("localhost");
+        connection.connect();
+        PubSubManager mgr = new PubSubManager(connection);
+        System.out.println("Verbunden: " + connection.isConnected());
+
+        //Testknoten  erzeugen
+        LeafNode node = mgr.getNode("testNode");
+        connection.disconnect();
+//        LeafNode leaf = mgr.createNode("testNode2");
+//        ConfigureForm form = new ConfigureForm(FormType.submit);
+//        form.setAccessModel(AccessModel.open);
+//        form.setDeliverPayloads(false);
+//        form.setNotifyRetract(true);
+//        form.setPersistentItems(true);
+//        form.setPublishModel(PublishModel.open);
+//        leaf.sendConfigurationForm(form);
+
+
+        //Zu Testknoten publishen
+//        LeafNode node = mgr.getNode("testNode");
+//        node.send(new Item());
+//
+    }
+}
+
