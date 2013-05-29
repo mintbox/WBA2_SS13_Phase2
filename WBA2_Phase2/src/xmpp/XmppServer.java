52af8417ec074de601ff0e9e2b8c33e24d179971
dennis.jga@gmail.com <juPtdvnIAS7io3xsJApGSGPZI>
2013-05-29 12:15:01 +0200

    REST_Server wiederhergestellt

--- WBA2_Phase2/src/xmpp/XmppServer.java
+++ WBA2_Phase2/src/xmpp/XmppServer.java
@@ -1,9 +1,8 @@
 package xmpp;
 
+
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
-import org.jivesoftware.smackx.pubsub.LeafNode;
-import org.jivesoftware.smackx.pubsub.PubSubManager;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,17 +13,11 @@ import org.jivesoftware.smackx.pubsub.PubSubManager;
  */
 public class XmppServer {
     public static void main(String[]args) throws XMPPException {
-        // Erzeugt Connection zum Server
         XMPPConnection connection = new XMPPConnection("localhost");
-// Connect to the server
         connection.connect();
-// Login auf dem Server
         connection.login("user1", "1234");
 
-// Erzeugt einen PubSubManager
-        PubSubManager mgr = new PubSubManager(connection);
 
-// Erzeugt Leaf
-        LeafNode leaf = mgr.createNode();
+
     }
 }
