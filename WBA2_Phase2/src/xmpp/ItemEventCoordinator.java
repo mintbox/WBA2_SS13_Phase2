7b108ee35884d0a2cefee608c93a1b815ba5d6db
denjae <dennis.jga@gmail.com>
2013-05-31 15:22:36 +0200

    ItemEventCoordinatior hinzugefuegt

---
+++ WBA2_Phase2/src/xmpp/ItemEventCoordinator.java
@@ -0,0 +1,20 @@
+package xmpp;
+
+import org.jivesoftware.smackx.pubsub.ItemPublishEvent;
+import org.jivesoftware.smackx.pubsub.listener.ItemEventListener;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: djga
+ * Date: 31.05.13
+ * Time: 15:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class ItemEventCoordinator<T> implements ItemEventListener {
+    @Override
+    public void handlePublishedItems(ItemPublishEvent items)
+    {
+        System.out.println("Item count: " + items.getItems().size());
+        System.out.println(items);
+    }
+}
--- WBA2_Phase2/src/xmpp/XmppSubClient.java
+++ WBA2_Phase2/src/xmpp/XmppSubClient.java
@@ -1,5 +1,11 @@
 package xmpp;
 
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smackx.pubsub.Item;
+import org.jivesoftware.smackx.pubsub.LeafNode;
+import org.jivesoftware.smackx.pubsub.PubSubManager;
+
 /**
  * Created with IntelliJ IDEA.
  * User: djga
@@ -8,8 +14,21 @@ package xmpp;
  * To change this template use File | Settings | File Templates.
  */
 public class XmppSubClient {
+    XMPPConnection connection;
+    PubSubManager mgr = new PubSubManager(connection);
 
-       public void abonniere() {
+    XmppSubClient(String user, String pass) throws XMPPException {
+        connection = new XMPPConnection("localhost");
+        connection.connect();
+        connection.login(user, pass);
+    }
+
+
+    public void abonniere(String team) throws XMPPException {
+        LeafNode node = mgr.getNode(team);
+
+        node.addItemEventListener(new ItemEventCoordinator<Item>());
+        node.subscribe(connection.getUser());
 
     }
 
